cb2cd0b354ce5b154c91a605898ead71266c6d46
SkyZ18 <114467762+SkyZ18@users.noreply.github.com>
2023-11-10 08:55:40 +0100

    init

---
+++ cbct-api/src/main/java/com/schwarzit/cbctapi/CbctApiApplication.java
@@ -0,0 +1,13 @@
+package com.schwarzit.cbctapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbctApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbctApiApplication.class, args);
+	}
+
+}
---
+++ cbct-api/src/test/java/com/schwarzit/cbctapi/CbctApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schwarzit.cbctapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbctApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
